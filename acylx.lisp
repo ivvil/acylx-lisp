2687873aef56952d621743abf4f37b61b9652a76
Iván Villagrasa Pérez <56278092+ivvil@users.noreply.github.com>
2024-02-21 13:59:45 +0100

    Fixed css font declaration generation

--- acylx.lisp
+++ acylx.lisp
@@ -13,19 +13,21 @@
   
   )
 
+;; Seems like spinneret translastes te quote marks inside style tags to &quot;
+
 (defmacro display-page (&body body)
   `(spinneret:with-html-string
 	 (:doctype)
 	 (:html
 	  (:head
 	   (acylx-head "SHFT.dev")
-	   (:style (concatenate 'string (lass:compile-and-write *main-css*) (apply 'concatenate 'string (get-fonts *font-dir*)))))
+	   (:style (:raw (apply 'lass:compile-and-write (append (list *main-css*) (get-fonts *font-dir*)))))
 	  (:body
-	   ,@body))))
+	   ,@body)))))
 
 (with-open-file (str "test.html"
                      :direction :output
                      :if-exists :supersede
                      :if-does-not-exist :create)
   (format str (display-page
-				(:h1 "hello world"))))
+	(:h1 "hello world"))))
--- css.lisp
+++ css.lisp
@@ -5,7 +5,7 @@
 (defparameter *main-css*
   '(body
 	:background-color "red"
-	:font-face "Starbirl"))
+	:font-family "Starbirl"))
 
 ;; @font-face {
 ;; 	font-family: "Starbirl";
@@ -27,13 +27,18 @@
 ;; 	(let ((name (cut-string-at-dot (file-namestring file))))
 ;; 	  (add-font file name))))
 
-(defun get-fonts (path)
-  (loop for file in (uiop:directory-files path) collect (let ((name (cut-string-at-dot (file-namestring file))))
-																			  (lass:compile-and-write (add-font file name)))))
+;; (defun get-fonts (path)
+;;   (loop for file in (uiop:directory-files path) collect (let ((name (cut-string-at-dot (file-namestring file))))
+;; 																			  (lass:compile-and-write (add-font file name)))))
+
+(defun get-fonts (path) "Gets all font declarations for a given folder"
+  (loop for file in (uiop:directory-files path) collect (let ((name (cut-string-at-dot (file-namestring file)))) (add-font file name))))
 
 ;; TODO Return lass code instead of css
 ;; TODO Return a string instead of an array of string
 ;; TODO Use local paths instead of global paths
 
 (defun add-font (path name)
-  `(:font-face :font-family ,name :src (url ,(namestring path))))
+  `(:font-face :font-family ,name :src (url ,(lass:resolve (namestring path)))))
+
+
